cf5576012ea08b3fc6ec99da9381bd08e85fa248
烟柳拂风 <13938963794@163.com>
2023-08-26 21:41:07 +0800

    Site updated: 2023-08-26 21:41:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/13560.html","posts/9072.html","posts/16107.html","posts/d50a.html","posts/41958.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/13560.html","posts/16107.html","posts/9072.html","posts/41958.html","posts/d50a.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
